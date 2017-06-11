fa001ef331dc3d7d5abf37ea3c61466fc6a7b8c4
robert.butacu@yahoo.com <robert.butacu@yahoo.com>
2017-06-11 11:50:42 +0300

    Added growing circles.

--- NepalEarthquake/target/NepalEarthQuakeLatest/resources/js/aftershocks.js
+++ NepalEarthquake/target/NepalEarthQuakeLatest/resources/js/aftershocks.js
@@ -2,20 +2,8 @@
  * Created by Robert-PC on 6/10/2017.
  */
 var url = "http://localhost:9999/aftershocks/detailed";
-window.onload = pageLoad;
-
-
-function pageLoad() {
-    var startButton = document.getElementById("start");
-
-    var xhttp = new XMLHttpRequest();
-     //var ourData=JSON.parse(xhttp.responseText);
-
-}
 
 function alertMe() {
-    strings = ["asd", "Asd", "asda", "asdasd"];
-
     var xhttp = new XMLHttpRequest();
     xhttp.addEventListener("load",function() {
         var i = 0;
@@ -36,8 +24,32 @@ function startAlerting(strings, i){
     document.getElementById("googleMap").innerHTML = "";
     var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
 
+    var cityCircle = new google.maps.Circle({
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#E16D65',
+        fillOpacity: 0,
+        map: map,
+        center: {lat : 28.502943, lng : 83.619921},
+        radius: 15000
+    });
+
+    var maxRadius = 100000;
+    var changeCircleRadius = setInterval(function(){
+
+        cityCircle.setRadius(cityCircle.getRadius() + 3000);
+
+        if(cityCircle.getRadius() > maxRadius){
+            clearInterval(changeCircleRadius);
+            cityCircle.setRadius(5000);
+            cityCircle.fillOpacity = 1;
+        }
+    }, 50);
+
+
     var testingInterval = setInterval(function(){
-        console.log(strings[i]);
+        //alert(strings[i]);
         i = i+1;
         if ( i === strings.length){
             i = 0;
@@ -48,7 +60,7 @@ function startAlerting(strings, i){
 
 function myMap() {
     var mapProp= {
-        center:new google.maps.LatLng(51.508742,-0.120850),
+        center:new google.maps.LatLng(28.502943, 83.619921),
         zoom:5
     };
     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
